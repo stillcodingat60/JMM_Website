50c4662a4b987408cc141ee75f5329fc42f1bdd5
stillcodingat60 <jamiemcconnell@hotmail.com>
2016-01-11 16:44:47 -0500

    mostly complete
    needs some add'l formating

---
+++ JMM_Website/assets/js/resume.js
@@ -0,0 +1,64 @@
+
+    function y2016() {
+        if ($('#2016').is(':hidden')) {
+            $('#2016').slideDown(1500);
+            $('#2016d').slideDown(1500);
+        } else {
+            $('#2016').hide(1500);
+            $('#2016d').hide(1500);
+        }
+    };
+
+    function y2002() {
+        if ($('#2002').is(':hidden')) {
+            $('#2002').slideDown(1500);
+            $('#2002d').slideDown(1500);
+        } else {
+            $('#2002').hide(1500);
+            $('#2002d').hide(1500);
+        }
+    };
+
+    function y1989() {
+        if ($('#1989').is(':hidden')) {
+            $('#1989').slideDown(1500);
+            $('#1989d').slideDown(1500);
+        } else {
+            $('#1989').hide(1500);
+            $('#1989d').hide(1500);
+        }
+    };
+
+    function y1985() {
+        if ($('#1985').is(':hidden')) {
+            $('#1985').slideDown(1500);
+            $('#1985d').slideDown(1500);
+        } else {
+            $('#1985').hide(1500);
+            $('#1985d').hide(1500);
+        }
+    };
+
+    function y1981() {
+        if ($('#1981').is(':hidden')) {
+            $('#1981').slideDown(1500);
+            $('#1981d').slideDown(1500);
+        } else {
+            $('#1981').hide(1500);
+            $('#1981d').hide(1500);
+        }
+    };
+
+    function yEDU() {
+        if ($('#edu').is(':hidden')) {
+            $('#edu').slideDown(1500);
+            $('#edud').slideDown(1500);
+        } else {
+            $('#edu').hide(1500);
+            $('#edud').hide(1500);
+        }
+    };
+    $(document).ready(function () {
+        console.log("in resume.js");
+        $('.event').hide();
+    });
---
+++ JMM_Website/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JMM_Website.MvcApplication" Language="C#" %>
